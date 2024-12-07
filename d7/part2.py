364e6ff29f2d0bef5adf63d640590d70de233039
AleFeri <50620354+AleFeri@users.noreply.github.com>
2024-12-07 21:21:30 +0100

    Completed day 7

---
+++ d7/part2.py
@@ -0,0 +1,30 @@
+from typing import Tuple
+from itertools import product
+
+def evaluate(numbers: list[int], operators: Tuple[str, ...]) -> int:
+    result = numbers[0]
+    for i in range(len(operators)):
+        if operators[i] == '+':
+            result += numbers[i + 1]
+        elif operators[i] == '*':
+            result *= numbers[i + 1]
+        elif operators[i] == '|':
+            result = int(f"{result}{numbers[i + 1]}")
+    return result
+
+with open('input.txt') as input_file:
+    lines = input_file.read().splitlines()
+
+result = 0
+for line in lines:
+    aspected_result, operands = line.split(": ")
+    aspected_result = int(aspected_result)
+    operands = list(map(int, operands.split()))
+    operations = product("+*|", repeat=len(operands)-1)
+
+    for op in operations:
+        if evaluate(operands, op) == aspected_result:
+            result += aspected_result
+            break
+
+print(result)
